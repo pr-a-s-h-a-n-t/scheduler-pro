bb52b53219a179f25f2b218817d96b8ae01a7255
Prashant Mishra <prashantmishra111@outlook.com>
2023-11-13 23:38:23 +0530

    protected route

--- src/issue.js
+++ src/issue.js
@@ -1,4 +1,4 @@
 //1. Duplicate accounts are being created and need to be fixed by the backend.
 //2. I need to get a task by its ID through an API.
 //3. login api not working need to be fixed.(fixed)
-//4. Issue in routing. protected routes is not working correctly.(working fine on localhost but throwing (404) on dp server.)
+//4. Issue in routing. protected routes is not working correctly.(working fine on localhost but throwing (404) on dp server.)(fixed)
